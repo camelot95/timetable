c5da3566164efdcb530afb04ea072648f7863f8c
kasper <kamalutfulin@gmail.com>
2022-07-12 00:12:33 +0300

    FIX: method checkCollisionTime

--- src/main/java/com/example/timetable/repositorie/MeetingRepository.java
+++ src/main/java/com/example/timetable/repositorie/MeetingRepository.java
@@ -14,12 +14,12 @@ public interface MeetingRepository extends JpaRepository<Meeting, Integer> {
     @Query(nativeQuery = true, value = "select exists (select 1 from meeting " +
             "where ((:meetingTimeStart) > meeting.meeting_time_start and (:meetingTimeStart)  < meeting_time_stop) " +
             "or ((:meetingTimeStop)  > meeting.meeting_time_start and (:meetingTimeStop) < meeting_time_stop) " +
+            "or ((:meetingTimeStart)  < meeting.meeting_time_start and (:meetingTimeStop) > meeting_time_stop) " +
             "and meeting.userid = (:userid))")
     Boolean checkCollisionTime(@Param("userid") Integer userid, @Param("meetingTimeStart") LocalDateTime meetingTimeStart,
                                @Param("meetingTimeStop") LocalDateTime meetingTimeStop);
 
-    @Query(nativeQuery = true, value = "select * from meeting where ((:meetingTimeStart) > meeting.meeting_time_start and (:meetingTimeStart)  < meeting.meeting_time_stop) " +
-            "or ((:meetingTimeStop)  > meeting.meeting_time_start and (:meetingTimeStop) < meeting.meeting_time_stop) ")
+    @Query(nativeQuery = true, value = "select * from meeting where ((:meetingTimeStart) < meeting.meeting_time_start and (:meetingTimeStop) > meeting.meeting_time_stop)")
     List<Meeting> findAllByMeetingTimeStartIsBetween(@Param("meetingTimeStart") LocalDateTime meetingTimeStart,
                                                      @Param("meetingTimeStop") LocalDateTime meetingTimeStop);
 }
